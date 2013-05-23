11bf2acbba2cb076169e2c9855853f3cc2faeaa7
Dan Anderson <dan.hillier.anderson@gmail.com>
2013-05-23 00:38:05 -0400

    Formatted and gutted include/general

--- include/general/searchhome.php
+++ include/general/searchhome.php
@@ -1,25 +1,83 @@
-<?php include "gtop1.php"; ?></div><br /><br />
-<div id="fn" class="w8"><form autocomplete="off" name="mowquery" method="post" action="search.php?do=now"><table 
-width="100%" 
-border="0" cellpadding="0" cellspacing="0"><tr style="height:20px"><td class="ll" colspan="3">&nbsp;</td><td class="mg">&nbsp;</td>
-<td class="rr" rowspan="3">Find members:<br />
-&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="vol" value="1" checked="checked" id="chkvol" />&nbsp;&nbsp;<label for="chkvol">Volunteers</label>
-&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="checkbox" name="client" value="1" checked="checked" id="chkclient" />&nbsp;&nbsp;<label for="chkclient">Client</label>
-<br /><br />Search by:&nbsp;&nbsp;<select name="qstyle">
-<option value="phone">Phone Number</option>
-<option value="address">Address</option>
-</select>
-
-</td></tr><tr id="sr"><td>&nbsp;</td><td class="rt"><img src="p1/sr_nd.gif" width="21" height="18" border="0" alt="" /></td>
-<td id="sf"><div style="width:248px;height:18px;"><input type="text" id="mq_sr" name="mquery" size="10" /></div>
-</td><td class="mg"><img src="p1/arr_r.gif" width="9" height="18" border="0" alt="" /></td></tr>
-<tr><td><img src="p1/apup.gif" width="161" height="74" border="0" alt="" /></td><td valign="top">&nbsp;</td><td class="rt"><img src="p1/fdbgr.gif" width="110" height="44" border="0" alt="" /></td><td class="mg">&nbsp;</td></tr></table></form></div><div id="sep" class="w8">&nbsp;</div>
-<div id="cn" class="w8"><div id="ca"><img src="p1/aplo.gif" width="164" height="66" border="0" alt="" /></div>
-<div id"ct"><table width="100%" border="0" cellpadding="0" cellspacing="0" style="clear:left">
-<tr>
-<td id="bp">&nbsp;</td>
-<td id="cp">&nbsp;</td></tr></table><br />
-
-</div></div></div>
-</center></body></html>
+<?php include "gtop1.php"; ?>
+<br/>
+<br/>
+<div class="w8" id="fn">
+ <form action="search.php?do=now" autocomplete="off" method="post" name="mowquery">
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+   <tr style="height:20px">
+    <td class="ll" colspan="3">
+    </td>
+    <td class="mg">
+    </td>
+    <td class="rr" rowspan="3">
+     Find members:
+     <br/>
+     <input checked="checked" id="chkvol" name="vol" type="checkbox" value="1"/>
+     <label for="chkvol">
+      Volunteers
+     </label>
+     <input checked="checked" id="chkclient" name="client" type="checkbox" value="1"/>
+     <label for="chkclient">
+      Client
+     </label>
+     <br/>
+     <br/>
+     Search by:
+     <select name="qstyle">
+      <option value="phone">
+       Phone Number
+      </option>
+      <option value="address">
+       Address
+      </option>
+     </select>
+    </td>
+   </tr>
+   <tr id="sr">
+    <td>
+    </td>
+    <td class="rt">
+     <img alt="" border="0" height="18" src="p1/sr_nd.gif" width="21"/>
+    </td>
+    <td id="sf">
+     <div style="width:248px;height:18px;">
+      <input id="mq_sr" name="mquery" size="10" type="text"/>
+     </div>
+    </td>
+    <td class="mg">
+     <img alt="" border="0" height="18" src="p1/arr_r.gif" width="9"/>
+    </td>
+   </tr>
+   <tr>
+    <td>
+     <img alt="" border="0" height="74" src="p1/apup.gif" width="161"/>
+    </td>
+    <td valign="top">
+    </td>
+    <td class="rt">
+     <img alt="" border="0" height="44" src="p1/fdbgr.gif" width="110"/>
+    </td>
+    <td class="mg">
+    </td>
+   </tr>
+  </table>
+ </form>
+</div>
+<div class="w8" id="sep">
+</div>
+<div class="w8" id="cn">
+ <div id="ca">
+  <img alt="" border="0" height="66" src="p1/aplo.gif" width="164"/>
+ </div>
+ <div id"ct">
+  <table border="0" cellpadding="0" cellspacing="0" style="clear:left" width="100%">
+   <tr>
+    <td id="bp">
+    </td>
+    <td id="cp">
+    </td>
+   </tr>
+  </table>
+  <br/>
+ </div>
+</div>
